2935c25e86058472fc59f500d47a647646d51752
coreyapat <coreyapat19@gmail.com>
2021-08-03 10:46:18 -0500

    Created a delete option, as well as a flashcard id

--- Flash/views.py
+++ Flash/views.py
@@ -23,7 +23,6 @@ class FlashCardsList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class FlashIdList(APIView):
 
     def filter_by_id(self, card):
@@ -46,3 +45,26 @@ class CreateCard(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FlashCardInfo(APIView):
+
+    def get_by_id(self, fk):
+        try:
+            return FlashCards.objects.get(fk=fk)
+        except FlashCards.DoesNotExist:
+            raise Http404
+
+    def put(self, request, fk):
+        flashCard_Id = self.get_by_id(fk)
+        updatedFlashCard = FlashCardsSerializer(flashCard_Id, data=request.data, partial=True)
+        if updatedFlashCard.is_valid():
+            updatedFlashCard.save()
+            return Response(updatedFlashCard.data, status=status.HTTP_202_ACCEPTED)
+        return Response(updatedFlashCard.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    def delete(self, request, fk):
+        flashCard_Id = self.get_by_id(fk)
+        deleteFlashCard = FlashCardsSerializer(flashCard_Id)
+        flashCard_Id.delete()
+        return Response(deleteFlashCard.data, status=status.HTTP_200_OK)
