107c56ad7a92af43567f198968e004b414710972
coreyapat <coreyapat19@gmail.com>
2021-08-03 11:02:45 -0500

    Created a url and created a new class

--- Flash/urls.py
+++ Flash/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('Flash/', views.FlashCardsList.as_view()),
     path('flashcard/<int:card>/', views.FlashCardsList.as_view()),
     path('flashcard/', views.CreateCard.as_view()),
-
+    path('flashcard/<int:fk>/', views.FlashCardInfo.as_view()),
 ]
